052cb7eb783bf6393c1842ce3b799428f6513acc
dakubota <36279775+dakubota@users.noreply.github.com>
2018-03-15 23:36:01 -0700

    Commented Code
    Made everything more clear.

--- PROJECT_2FINAL.m
+++ PROJECT_2FINAL.m
@@ -35,7 +35,7 @@ avgDigit(10,:) = getAvg(train0);
 % Set up multilayer network
 tR = 0.05;                  % Value between [0.01,1]
 layers = [784 50 50 50 50 10];   % 1st entry is input layer size, middle are 
-                           % are hidden layer size, last is output size.
+                                 % are hidden layer size, last is output size.
 n = length(layers);
   % Set up input matrices
   for i=1:n
@@ -59,70 +59,59 @@ n = length(layers);
   
 TARGET = eye(10);
 % Set up training data  
+% Creates a cell filled with all of the training data from MNIST database.
 trainData = {train0,train1,train2,train3,train4,train5,train6,train7,train8,train9};
+% Creates a cell filled with all of the testing data from the MNIST database.
 testData = {test0,test1,test2,test3,test4,test5,test6,test7,test8,test9};
-perRight = zeros(1,10);
-score=0;
 
-saveOut = zeros(10,10);
+saveOut = zeros(10,10); % Creates a matrix of zeros to store the last entry from each set of training data.
 
 
 % TRAIN THE NETWORK
 
 for numI=1:10 %Iterate through the sets of training data 0-9
-  [numD, numPix] = size(trainData{numI});
-  [numT, numPix] = size(testData{numI});
+  [numD, numPix] = size(trainData{numI}); % numD is the total number of data to train for each cell of trainData.
+  [numT, numPix] = size(testData{numI});  % numT is the total number of data to test for each cell of testData.
   data = trainData{numI};     % Counts through training data numbers
   dataT = testData{numI};     % Counts through test data numbers
   %TRAIN LOOP 
-  for countD=1:1000%numD
-    inC{1} = im2double(data(countD,:));  % Counts through train_n entries
-
+  for countD=1:numD           % Counts throught total number of data points.
+    inC{1} = im2double(data(countD,:));  % Counts through train_n entries.
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Forward
-    for countLayer=1:n-1
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Iterates through the layers storing the output as the next input.
+    for countLayer=1:n-1  
       inC{countLayer+1} = neuron(inC{countLayer},wiC{countLayer});
     end
-
-    % Backward
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % BACKWARD PROPAGATION
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     % OUTPUT LAYER
+    % Calculates the change in weight for the output layer.
     ERROR = TARGET(numI,:)-inC{n};
     deltaC{n-1} = inC{n}.*(1-inC{n}).*ERROR;
     dw{n-1} = tR.*(deltaC{n-1}'*inC{n-1})';
     wiC{n-1} = wiC{n-1} + dw{n-1};
+    
     % HIDDEN LAYERS
+    % Calculates the change in weight for the hidden layers.
     for i=n-2:-1:1
       deltaC{i} = (deltaC{i+1}*wiC{i+1}').*(inC{i+1}.*(1-inC{i+1}));
       dw{i} = tR.*(deltaC{i}'*inC{i})';
       wiC{i} = wiC{i}+dw{i};
     end
+  
+    % Saves the last entry of the training data per digit.
+    if countD==numD
+      saveOut(numI,:) = inC{n}; % Save output for last entry of each didgit
+    end
+    
+  end
 
-    for i=1:n-1
-      wiC{i} = wiC{i}+dw{i};
-    end
-    if countD==1000%numD
-      saveOut(numI,:) = inC{n};
-    end
-    
-  end
-  score=0;
-  for countT=1:numT
-    inC{1} = im2double(dataT(countT,:));
-    
-    for countLayer=1:n-1
-      inC{countLayer+1} = neuron(inC{countLayer},wiC{countLayer});
-    end
-    A = inC{n};
-    [x, y]=max(A(1,:));
-    if y==numI
-      score = score+1;
-    end
-  end
-  perRight(1,numI) = 1-(score/numT);
 end
 
 
 disp('done')
-
-
-
